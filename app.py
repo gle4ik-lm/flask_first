2254354cdb7f50103ba38e1cac6b8ab8056193df
Gle4ik <mamontovgeorgij0@gmail.com>
2026-06-24 20:53:19 +0300

    add main page and menu page

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
+current_time = datetime.datetime.now()
+is_open = "11:00:00" < current_time.strftime("%H:%M:%S" ) < "22:00:00"
+today = datetime.datetime.now().weekday()
+is_friday = True if today == 4 else False
+
+
+
+menu = {
+"Капучино": 80,
+"Лате": 85,
+"Еспресо": 60,
+"Чізкейк": 120,
+"Панкейки": 95
+}
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', status = is_open)
 
-@app.route('/for_student')
-def for_student():
-    return render_template('for_student.html', name = 'Lera')
+@app.route('/menu')
+def coffee_shop_menu():
+    return render_template('menu.html', status = is_friday, menu = menu)
+
 app.run(debug=True)
+
