158666dd2e4ca45ffd85b0d5e724631469196d3c
Gle4ik <mamontovgeorgij0@gmail.com>
2026-06-25 12:30:16 +0300

    chage main and menu page code

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
 import datetime
 app = Flask(__name__)
-current_time = datetime.datetime.now()
-is_open = "11:00:00" < current_time.strftime("%H:%M:%S" ) < "22:00:00"
-today = datetime.datetime.now().weekday()
-is_friday = True if today == 4 else False
+
+
 
 
 
@@ -18,11 +16,19 @@ menu = {
 
 @app.route('/')
 def index():
-    return render_template('index.html', status = is_open)
+    current_time = datetime.datetime.now()
+    is_open = "11:00:00" < current_time.strftime("%H:%M:%S" ) < "22:00:00"
+
+    return render_template('index.html', status=is_open)
+
 
 @app.route('/menu')
 def coffee_shop_menu():
-    return render_template('menu.html', status = is_friday, menu = menu)
+
+    today = datetime.datetime.now().weekday()
+    is_friday = True if today == 4 else False
+    return render_template('menu.html', status=is_friday, menu=menu)
+
 
 app.run(debug=True)
 
