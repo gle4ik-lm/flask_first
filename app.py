2927907bf6de2a2d84fe098ffa3a52dee364062f
Gle4ik <mamontovgeorgij0@gmail.com>
2026-06-25 23:09:24 +0300

    make home page

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = '***'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+
+
+        flash(f'Hello, {name}!')
+
+        return redirect(url_for('index'))
+    return render_template('index.html')
 
 app.run(debug=True)
